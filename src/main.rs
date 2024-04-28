91a5a9a92b6ac1097aa765d3e8fa1b726db49801
Tamion <70228790+notTamion@users.noreply.github.com>
2024-04-28 12:29:55 +0200

    some color

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use std::{env, fs};
+use colored::{ColoredString, Colorize};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let patch = fs::read_to_string(&args[1]).expect(format!("Wasn't able to read file {}", args[1]).as_str());
 
-    let mut simplified_patch = String::new();
+    let mut simplified_patch = ColoredString::from("");
     for line in patch.lines() {
         match line.chars().next().unwrap_or(' ') {
             '+' => {
                 let mut s = line.to_string();
                 s.remove(0);
-                simplified_patch.push_str(format!(" {}\n", s.as_str()).as_str())
+                simplified_patch = format!("{} {}\n", simplified_patch, s.as_str()).green();
             },
             '-' => {}
-            _ => simplified_patch.push_str(format!("{}\n", line).as_str())
+            _ => simplified_patch = format!("{}{}\n", simplified_patch, line).white()
         }
     }
 
